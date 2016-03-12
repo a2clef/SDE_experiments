f8a706e08b0fafdf3a75e38160a5b84bb8881699
aclef <aclef@qq.com>
2016-03-12 21:34:41 +0800

    finished kmp ver1

--- SDE/1-2-kmp/1-2-kmp.cpp
+++ SDE/1-2-kmp/1-2-kmp.cpp
@@ -9,11 +9,41 @@ int kmp(std::string &input, std::string &pattern)
 	int length_input = input.length();
 	int length_pattern = pattern.length();
 	int next[MAX_LENGTH_PATTERN];
+	int position = 0;
 	//calculate the next array
+	/*
+	SOME THING MAGIC HAPPENS HERE
+	TAKES ME 10 MINUTES TO UNDERSTAND ONLY ONE LINE OF CODE
+	*/
+	int max_match = 0;
+	next[0] = 0;
+	for (int i = 1; i < length_pattern; i++)
+	{
+		/*
+		the magic while loop
+		if the new character mismatched, 
+		*/
+		while (max_match && (pattern[i] != pattern[max_match])) 
+			max_match = next[max_match - 1]; 
 
+		if (pattern[i] == pattern[max_match])
+			max_match++;
+		next[i] = max_match;
+	};
+	//==================
+	std::cout << "The next array for the pattern is:" << std::endl;
+	for (int i = 0; i < length_pattern; i++) std::cout << next[i] << " ";
+	std::cout << std::endl;
 	//kmp match using next
-
-
+	for (int i = 0; i < length_input; i++)
+	{
+		while (position && pattern[position] != input[i])
+			position = next[position - 1];
+		if (pattern[position] == input[i])
+			position++;
+		if (position == length_pattern)
+		return (i - length_pattern + 1);
+	}
 	//code  only reachs here if pattern is not found in the string
 	return -1;
 };
